9df49f96b383efcf49afe9dc820af6b7e67e6a0e
ibad nurhamim <nurhamimibad96@gmail.com>
2021-01-11 17:12:40 +0700

    [TUTORIAL] Materi belajar maven -> menambahkan depedency dan repository pada pom.xml

---
+++ src/main/java/com/tutorial/maven/App.java
@@ -0,0 +1,20 @@
+package com.tutorial.maven;
+
+import com.google.gson.Gson;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Gson gson = new Gson();
+
+        Person person = new Person("Ibad Nurhamim, sedang belajar apache maven");
+        String json = gson.toJson(person);
+
+        System.out.println(json);
+    }
+}
